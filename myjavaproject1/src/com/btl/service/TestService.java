f8224bc7b5b5ad32bcadd614c3946d8f58cbc5ac
sajeev <sajeev.training@gmail.com>
2013-04-23 11:25:52 +0100

    initial

---
+++ myjavaproject1/src/com/btl/service/TestService.java
@@ -0,0 +1,14 @@
+package com.btl.service;
+
+
+// initial version
+
+public class TestService {
+	
+	
+	public static void main (String a[]){
+		
+		System.out.println("This for testing git hub repository");
+	}
+
+}
